5fed9cb891e16877ba78c1c3d7076f1ce9c3eae9
Oscar Alonso <oscar@MacBook-Pro-de-Oscar.local>
2020-10-12 19:09:16 +0200

    feat: menu basic w/o game scene

---
+++ Shata/Assets/Common/Scripts/AudioHelper.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Common
+{
+    public class AudioHelper : MonoBehaviour {
+
+        AudioSource audioSource;
+
+        void Start () {
+            audioSource = GetComponent<AudioSource>();
+        }
+	
+
+        public void PlaySound(AudioClip audioClip) {
+            if (audioSource == null || audioClip == null) return;
+
+            audioSource.PlayOneShot(audioClip);
+        }
+    }
+}
---
+++ Shata/Assets/Menu/Scripts/MainMenuController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using Common;
+
+namespace Menu
+{
+    public class MainMenuController : MonoBehaviour {
+
+        [SerializeField] GameObject creditsMenu;
+        [SerializeField] GameObject mainMenu;
+        [SerializeField] AudioClip clickAudio;
+
+        AudioHelper audioHelper;
+        
+        void Start() {
+            audioHelper = GetComponent<AudioHelper>();
+            mainMenu.SetActive(true);
+            creditsMenu.SetActive(false);
+        }
+
+        public void NewGame() {
+            audioHelper.PlaySound(clickAudio);
+            SceneManager.LoadScene("Tutorial");
+        }
+
+        public void ShowCredits() {
+            audioHelper.PlaySound(clickAudio);
+            creditsMenu.SetActive(true);
+            mainMenu.SetActive(false);
+        }
+
+        public void ExitGame() {
+            audioHelper.PlaySound(clickAudio);
+            Application.Quit();
+        }
+        
+        public void GoBack() {
+            audioHelper.PlaySound(clickAudio);
+            mainMenu.SetActive(true);
+            creditsMenu.SetActive(false);
+        }
+
+    }
+
+}
