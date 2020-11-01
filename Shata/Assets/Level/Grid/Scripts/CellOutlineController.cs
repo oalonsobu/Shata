1a9a862425447d2d661329f9ecf5b02544a47141
Oscar Alonso <oscar@MacBook-Pro-de-Oscar.local>
2020-11-01 10:48:45 +0100

    feat: mini refactor cell reference and build house

---
+++ Shata/Assets/Level/Grid/Scripts/CellOutlineController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Variables;
+
+
+
+//TODO: convertir esto en una clase sin logica, que solo responda a eventos varios y llame a lo que pertoca. Sera el CellReference
+namespace Level.Grid
+{
+    //Cell information related with unity behaviours
+    public class CellController : MonoBehaviour
+    {
+        Outline outline;
+        
+        [SerializeField] CellReference cellReference;
+        [SerializeField] GameEvent selectedCellChangedEvent;
+        private CellTypeInterface cell;
+        
+        void Start() {
+            outline = GetComponent<Outline>();
+            outline.enabled = false;
+        }
+        
+        public GameObject SelectCell () {
+            outline.enabled = true;
+            cellReference.value = cell;
+            selectedCellChangedEvent.Raise();
+            
+            return gameObject;
+        }
+        
+        public void UnselectCell () {
+            cellReference.value = null;
+            outline.enabled = false;
+            selectedCellChangedEvent.Raise();
+        }
+        
+        public void SetCell (CellTypeInterface c)
+        {
+            cell = c;
+        }
+    }
+}
---
+++ Shata/Assets/UI/Scripts/ConstructionPanelController.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Level.Grid;
+using UnityEngine;
+using UnityEngine.UI;
+using Variables;
+
+namespace UI
+{
+    public class BuildingTypePanelController : MonoBehaviour
+    {
+    
+        [SerializeField] Text titleText;
+        [SerializeField] Text descriptionText;
+        [SerializeField] Text commentText;
+      
+        GameObject container;
+    
+        private void Start() {
+            container = transform.GetChild(0).gameObject;
+            container.SetActive(false);
+        }
+    
+        public void selectedCellChangedEvent(CellReference cellReference)
+        {
+            bool isActive = cellReference.value != null && cellReference.value.AllowedBuildings.Any();
+            container.SetActive(isActive);
+            if (isActive) {
+                titleText.text = cellReference.value.Building.Title;
+                descriptionText.text = cellReference.value.Building.Description;
+                commentText.text = cellReference.value.Building.Comment;
+                
+            }
+        }
+    }
+}
