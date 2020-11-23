e6c712e468833c60841ca6f938959a2c6cbff159
Oscar Alonso <oscar@MacBook-Pro-de-Oscar.local>
2020-11-24 00:50:25 +0100

    fix: typo

--- Shata/Assets/Level/Cell/Scripts/Grass.cs
+++ Shata/Assets/Level/Cell/Scripts/Grass.cs
@@ -8,7 +8,7 @@ namespace Level.Cell
         private List<BuildingInterface> allowedBuildings;
         
         public override string Description => "Ideal for ranching or random buildings.";
-        public override string Comment => "Plenty of flowers, herbs and grass. Pettry but useless if you don't build something here.";
+        public override string Comment => "Plenty of flowers, herbs and grass. Pretty but useless if you don't build something here.";
         public override string Title => "Grassland";
         public override string BasePrefab => "GrassCell";
 
